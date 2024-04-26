0a6091a7203b6ec43c62c7d926bb335b1b492cd4
Kirill <b1nc0d3.kka@gmail.com>
2024-04-26 19:48:53 +0300

    added env file

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 LOGS_PATH = 'logs.txt'
 
 DB_DIR = 'db'
@@ -5,7 +12,7 @@ DB_NAME = 'gpt_helper.db'
 DB_TABLE_PROMPTS_NAME = 'prompts'
 
 ADMIN_ID = ''
-TOKEN = '6781055175:AAF-PaMGRcy21qn0aOPlpTLIz5wFO7cfhrc'
+TOKEN = os.getenv('TG_TOKEN')
 API_TOKEN = 't1.9euelZqVyZnKks-Rzo6SnJCJl5LNle3rnpWaisaTlpDNm8yTjYmYlpiayMvl8_dQRwRQ-e9mCExc_d3z9xB2AVD572YITFz9zef1656VmsjNjZiRlMmNnZORjs2elouY7_zF656VmsjNjZiRlMmNnZORjs2elouYveuelZrMjcbNzZzOy5OJipOZy4mXzbXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.CpqGiwwWxmVqHINDEpcsctuGOjzer0qw_2-PRfIiD3y9A6lSczWbqz1xRUsgGA1ol3tIL4KokHDHojBdJ4oYDg'
 
 FOLDER_ID = 'b1ghfb4epm2t95ql7k0d'
